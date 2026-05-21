91e91788823374717a4280287b4d1b970608e335
akselvassvik-maker <aksel.vassvik@gmail.com>
2026-05-21 12:59:50 +0200

    67

--- Prosjekt-V26/app.js
+++ Prosjekt-V26/app.js
@@ -1,19 +1,11 @@
 function lesMer(id, knapp) {
-
     const tekstEl = document.getElementById(id);
-
     if (tekstEl.style.display == "block") {
-
         tekstEl.style.display = "none";
         knapp.innerHTML = "Les mer ↓";
-
-    }
-
-    else {
-
+    } else {
         tekstEl.style.display = "block";
         knapp.innerHTML = "Les mindre ↑";
-
     }
 
 }
