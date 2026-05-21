4232ecfb8be03420f24b19d7b04ef8ba34be6a80
akselvassvik-maker <aksel.vassvik@gmail.com>
2026-05-21 22:17:30 +0200

    JS

--- Prosjekt-V26/app.js
+++ Prosjekt-V26/app.js
@@ -1,15 +1,27 @@
-function lesMer(id, knapp) {
-    const tekstEl = document.getElementById(id);
-    if (tekstEl.style.display == "block") {
-        tekstEl.style.display = "none";
-        knapp.innerHTML = "Les mer ↓";
-    } else {
-        tekstEl.style.display = "block";
-        knapp.innerHTML = "Les mindre ↑";
-    }
+
+
+const knapper = document.querySelectorAll(".knappen");
+const tekster = document.querySelectorAll(".ekstra-tekst");
+
+for (let i = 0; i < knapper.length; i++) {
+
+    knapper[i].addEventListener("click", function () {
+
+        if (tekster[i].style.display == "block") {
+
+            tekster[i].style.display = "none";
+            knapper[i].innerHTML = "Les mer ↓";
+
+        } else {
+
+            tekster[i].style.display = "block";
+            knapper[i].innerHTML = "Les mindre ↑";
+
+        }
+
+    });
 
 }
-
 
 function byttModus() {
 
